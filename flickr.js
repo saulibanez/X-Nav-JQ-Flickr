0951e956d157c559c27ed35d2f31845705f8556a
saulibanez <saul.ibanez.cerro@gmail.com>
2016-04-10 17:19:08 +0200

    flickr V1

---
+++ flickr.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$("#button").click(function() {
+		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?tags=fuenlabrada&tagmode=any&format=json&jsoncallback=?";
+		console.log("fots urjc");
+		$.getJSON( flickerAPI, {
+			tagmode: "any",
+			format: "json"
+		})
+		.done(function( data ) {
+			$.each( data.items, function( i, item ) {
+				$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+				if ( i === 15 ) {
+			        return false;
+				}
+	      	});
+	    });
+	})();
+});
