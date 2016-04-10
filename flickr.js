37e472bb62204ff74623f104c9027a8a002c6088
saulibanez <saul.ibanez.cerro@gmail.com>
2016-04-10 17:35:22 +0200

    flickr V2

--- flickr.js
+++ flickr.js
@@ -14,5 +14,22 @@ $(document).ready(function(){
 				}
 	      	});
 	    });
-	})();
+	});
+
+	$("#button2").click(function() {
+		var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + $("#search").val() + "&tagmode=any&format=json&jsoncallback=?";
+		console.log("fots urjc2");
+		$.getJSON( flickerAPI, {
+			tagmode: "any",
+			format: "json"
+		})
+		.done(function( data ) {
+			$.each( data.items, function( i, item ) {
+				$( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+				if ( i === 15 ) {
+			        return false;
+				}
+	      	});
+	    });
+	});
 });
